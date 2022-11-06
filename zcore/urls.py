63efbba660e1a7c434fe5d35284fa926cfdddeaa
Joshua <cutejosh2@gmail.com>
2022-11-06 02:42:35 +0100

    added support for swagger docs

--- zcore/urls.py
+++ zcore/urls.py
@@ -19,13 +19,34 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="App's API",
+      default_version='v1',
+      description="Some description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@apps.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", views.home, name="home"),
     path("api/user/", views.get_user_details, name="get_user_details"),
     path("api/auth/", include("authentication.urls")),
+
+    # docs
+    path('swagger(<format>\.json|\.yaml)/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
